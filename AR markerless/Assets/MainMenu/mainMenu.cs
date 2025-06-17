76ca2b31108f40beede1ba50be375fc01f7bf88b
nihlahh <mutiaranihlah@gmail.com>
2025-06-17 20:56:56 +0700

    mainmenu

---
+++ AR markerless/Assets/MainMenu/mainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Pastikan untuk mengimpor namespace ini
+
+public class MainMenu : MonoBehaviour
+{
+    public void TombolKeluar()
+    {
+        Application.Quit();
+        Debug. Log("Game Close");
+    }
+
+    public void Mainkan()
+    {
+        SceneManager. LoadScene("startAr");
+    }
+
+    public void Informasi()
+    { 
+        SceneManager. LoadScene("informasi");   
+    }
+
+    public void Credit()
+    {
+        SceneManager.LoadScene("credit");
+    }  
+}
+
---
+++ AR markerless/Assets/credit/credit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Pastikan untuk mengimpor namespace ini
+
+public class credit : MonoBehaviour
+{
+    public void Hyperlink()
+    {
+        SceneManager.LoadScene("mainmenu");
+    }  
+}
+
