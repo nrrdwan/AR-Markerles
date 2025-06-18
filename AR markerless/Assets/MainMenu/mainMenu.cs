63580fc02e85c7b635e08c0db8de5eb79ae33b4a
Nur Ridwan <nurridwan270305@gmail.com>
2025-06-18 13:17:15 +0700

    add table

--- AR markerless/Assets/MainMenu/mainMenu.cs
+++ AR markerless/Assets/MainMenu/mainMenu.cs
@@ -13,7 +13,7 @@ public class MainMenu : MonoBehaviour
 
     public void Mainkan()
     {
-        SceneManager. LoadScene("startAr");
+        SceneManager. LoadScene("kursi");
     }
 
     public void Informasi()
